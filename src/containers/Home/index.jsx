78cedc41310dc0e8a770167fa884d21eeabbe1ac
leandroricke100 <leandro_henrique_100@hotmail.com>
2023-07-22 12:37:38 -0300

    first commit

---
+++ src/containers/Home/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>Essa é a Home</p>
+    </div>
+  )
+}
+export default Home
---
+++ src/containers/Series/index.jsx
@@ -0,0 +1,9 @@
+function Series() {
+  return (
+    <div>
+      <h1>Series</h1>
+      <p>Essa é a Series</p>
+    </div>
+  )
+}
+export default Series
